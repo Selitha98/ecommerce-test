c61ea99d42a65c4f329d59c3b19ac60aa1cf3a72
Selitha98 <selithasasmitha@gmail.com>
2024-12-01 18:42:13 +0530

    did some changes in login and register pages

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Register() {
   return (
@@ -78,9 +79,9 @@ function Register() {
               </button>
               <p className="text-sm font-light text-gray-500 dark:text-gray-400">
                 Already have an account?{" "}
-                <a href="#" className="font-medium text-primary-600 hover:underline dark:text-primary-500">
+                <Link to={'/login'} className="font-medium text-primary-600 hover:underline dark:text-primary-500">
                   Login here
-                </a>
+                </Link>
               </p>
             </form>
           </div>
